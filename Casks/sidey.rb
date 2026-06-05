1c040eb320ded5b777b9514a673c02e349e213e6
Tao <chentao1006@gmail.com>
2026-06-06 00:52:01 +0800

    Update sidey to 1.2.4

--- Casks/sidey.rb
+++ Casks/sidey.rb
@@ -1,6 +1,6 @@
 cask "sidey" do
   version "1.2.4"
-  sha256 "baa82b96f3cb0c8b131793bc9fe32b3c32c91889a1268bedcbb087749bc31110"
+  sha256 "f100fc67a3072d8b466de54dbb0efa203029dbc634b5ebc905bc41ba172f9bee"
 
   url "https://github.com/chentao1006/sidey/releases/download/v1.2.4/Sidey.dmg"
   name "Sidey"
