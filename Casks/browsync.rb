e16f8eeb11df29e073088fcc79c8c127932bf066
Tao <chentao1006@gmail.com>
2026-06-15 16:01:28 +0800

    Update browsync to 1.0.10

--- Casks/browsync.rb
+++ Casks/browsync.rb
@@ -1,6 +1,6 @@
 cask "browsync" do
   version "1.0.10"
-  sha256 "26ecd59b88210bfa24e5406fca1258dc91ea240765f2d1c1cfe14f54dae8dedb"
+  sha256 "d59b6177912d6fbed8afd35278d06b98e1a574121e2a9517c0117f37df59eb3a"
 
   url "https://github.com/chentao1006/browsync/releases/download/v1.0.10/BrowSync.dmg"
   name "BrowSync"
