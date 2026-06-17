afcda403b0524e8c678d8c4f32ae1f57055d27fa
Tao <chentao1006@gmail.com>
2026-06-17 15:43:19 +0800

    Update flux-monitor to 1.3.6

--- Casks/flux-monitor.rb
+++ Casks/flux-monitor.rb
@@ -1,8 +1,8 @@
 cask "flux-monitor" do
-  version "1.3.5"
-  sha256 "8b9e493b25096279f7095255c8714ad65ef8dc7b03b3ac0457c1aa4c240dbf8c"
+  version "1.3.6"
+  sha256 "73807e8385932b41915546d662120a07af65763b8f9bf86c622878d6fc9d2420"
 
-  url "https://github.com/chentao1006/FluxMonitor/releases/download/v1.3.5/FluxMonitor.dmg"
+  url "https://github.com/chentao1006/FluxMonitor/releases/download/v1.3.6/FluxMonitor.dmg"
   name "Flux Monitor"
   desc "Server monitoring and management panel"
   homepage "https://github.com/chentao1006/FluxMonitor"
