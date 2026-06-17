8bcfb0b2aa4eed7cfa21a721c3caae876a845305
Tao <chentao1006@gmail.com>
2026-06-17 15:25:30 +0800

    Update browsync to 1.0.11

--- Casks/browsync.rb
+++ Casks/browsync.rb
@@ -1,8 +1,8 @@
 cask "browsync" do
-  version "1.0.10"
-  sha256 "df5fbde8e7388887aebe92e2ba84665e83558172300e7b23762ac54a690b66b4"
+  version "1.0.11"
+  sha256 "5f83a79fd4acd1d2f23fcab9b0fb7c7fd709b0afd8ae9c8a6627d575670434a5"
 
-  url "https://github.com/chentao1006/browsync/releases/download/v1.0.10/BrowSync.dmg"
+  url "https://github.com/chentao1006/browsync/releases/download/v1.0.11/BrowSync.dmg"
   name "BrowSync"
   desc "Unified browsing experience across multiple browsers"
   homepage "https://github.com/chentao1006/browsync"
