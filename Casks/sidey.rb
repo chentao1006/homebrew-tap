2b1dd36a9d967a9159547a25877ec4c7f4b985c1
Tao <chentao1006@gmail.com>
2026-07-30 13:47:13 +0800

    Update sidey to 1.3.0

--- Casks/sidey.rb
+++ Casks/sidey.rb
@@ -1,8 +1,8 @@
 cask "sidey" do
-  version "1.2.9"
-  sha256 "d46163e0d054addbb721f025a4f59ec7f4b75fdf7abfb389216972a1fe248a60"
+  version "1.3.0"
+  sha256 "73347efd7c5fbe35c274125067a45142e4ea459d9d419e438907123ca619d157"
 
-  url "https://github.com/chentao1006/sidey/releases/download/v1.2.9/Sidey.dmg"
+  url "https://github.com/chentao1006/sidey/releases/download/v1.3.0/Sidey.dmg"
   name "Sidey"
   desc "Context-aware AI assistant"
   homepage "https://github.com/chentao1006/sidey"
