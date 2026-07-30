b04155075d05073ab1adaa248eae1439822461f6
Tao <chentao1006@gmail.com>
2026-07-30 14:07:36 +0800

    Update sidey to 1.3.0

--- Casks/sidey.rb
+++ Casks/sidey.rb
@@ -1,6 +1,6 @@
 cask "sidey" do
   version "1.3.0"
-  sha256 "73347efd7c5fbe35c274125067a45142e4ea459d9d419e438907123ca619d157"
+  sha256 "61a987afc30c964248fd3c7ed519be770d5506c58bff9b1fa0613184abe47119"
 
   url "https://github.com/chentao1006/sidey/releases/download/v1.3.0/Sidey.dmg"
   name "Sidey"
